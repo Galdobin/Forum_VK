a3e6930ede55f9d0fc6312dbc67cfe50649d1dc8
Svity <svity@inbox.ru>
2012-11-22 17:04:39 +0400

    Добавили документацию

---
+++ api/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["f","Connect()"],["f","PrintGET()"],["f","PrintQuery()"],["f","PrintQueryLink()"],["f","PrintRawTwo()"],["f","ViewRubric1()"],["f","ViewRubric2()"],["f","ViewTopic1()"],["f","ViewUser1()"]];
